1a157377f0558bfbab82006bf4cc3824157bee04
Gavin Dinubilo <Gavin@gavins-air.attlocal.net>
2015-03-10 21:41:29 -0700

    update styles and such

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -21,5 +21,23 @@ $(document).ready(function() {
 		} else {
 			$("nav").removeClass("dark");
 		}
-	})
+	});
+
+	
+	function tmcc() {
+		console.log("TMCC");
+	}
+	function gbc() {
+		console.log("GBC");
+	}
+	function csn() {
+		console.log("CSN");
+	}
+
+	page.base('/');
+
+	page("/truckee-meadows-community-college", tmcc);
+	page("/great-basin-college", gbc);
+	page("/college-of-southern-nevada", csn);
+	page({hashbang: true});
 });
