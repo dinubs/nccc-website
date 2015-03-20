b6ac6e3a245c113ba1de9659d6dcde682a4156de
Gavin Dinubilo <Gavin@gavins-air.attlocal.net>
2015-03-19 22:20:29 -0700

    TMCC page setup

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,7 +2,7 @@
 //= require_tree .
 
 $(document).ready(function() {
-	$('a[href*=#]:not([href=#])').click(function() {
+	$('a[href*=#]:not([href=#]):not([href=#])').click(function() {
 	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 	      var target = $(this.hash);
 	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -14,30 +14,4 @@ $(document).ready(function() {
 	      }
 	    }
 	});
-	$(window).scroll(function() {
-		console.log($(this).scrollTop());
-		if($(this).scrollTop() > $(".top").height()) {
-			$("nav").addClass("dark");
-		} else {
-			$("nav").removeClass("dark");
-		}
-	});
-
-	
-	function tmcc() {
-		console.log("TMCC");
-	}
-	function gbc() {
-		console.log("GBC");
-	}
-	function csn() {
-		console.log("CSN");
-	}
-
-	page.base('/');
-
-	page("/truckee-meadows-community-college", tmcc);
-	page("/great-basin-college", gbc);
-	page("/college-of-southern-nevada", csn);
-	page({hashbang: true});
 });
